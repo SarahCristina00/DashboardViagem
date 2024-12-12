e42919614d23b26cdc30488239c5bd8ccc917b85
Lara Dias <lara.dias@estudante.ufjf.br>
2024-12-11 22:10:10 -0300

    adiciona biblioteca de icones (react-icons/fa)

--- src/Boas.tsx
+++ src/Boas.tsx
@@ -1,7 +1,15 @@
+import { FaSearchLocation } from "react-icons/fa"; //icone de pesquisa
+import { BsSuitcaseFill } from "react-icons/bs"; //icone de mala
+import "./Boas.css";
+
+
 function Boas(){
     return(
         <div className="boasVindas">
+            <BsSuitcaseFill className="icone"/>
             <p>Bem vinde, Agnes!</p>
+            <button className="pesquisar"><FaSearchLocation />
+            </button>
             <button className="adicionar">Adicionar Viagem</button>
         </div>
     )
