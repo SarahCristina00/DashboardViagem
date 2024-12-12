72ef0ec8b02db8f055c8706e27d62752b6b38fb4
Lara Dias <lara.dias@estudante.ufjf.br>
2024-12-11 21:02:30 -0300

    cria arquivos Boas.tsx e Boas.css e adiciona botão

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import './App.css'
+import './App.css';
+import Boas from './Boas';
 
 function App() {
-  
+  return(
+    <>
+      <h1>Dashboard</h1>
+      <Boas/>
+      </>
+  )
 }
 
 export default App
---
+++ src/Boas.tsx
@@ -0,0 +1,10 @@
+function Boas(){
+    return(
+        <div className="boasVindas">
+            <p>Bem vinde, Agnes!</p>
+            <button className="adicionar">Adicionar Viagem</button>
+        </div>
+    )
+}
+
+export default Boas
