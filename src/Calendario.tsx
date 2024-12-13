1fc1669337d70a75e2d5cea3352bbf25043753f8
wilian santos <14809032647@estudante.ufjf.br>
2024-12-13 01:11:31 -0300

    adiciona estilo para o calendario

--- src/Calendario.tsx
+++ src/Calendario.tsx
@@ -4,7 +4,9 @@ function Data(){
     return (
     <div className="Calendario">
     <h1>Calendário de Viagens</h1>
-    <label>Data Ida</label> 
+    
+    <label>Data Ida</label>
+    
     <input type="date" />
     <br></br>
     <label>Data Volta</label>
