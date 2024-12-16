5306495b538f562b6199b297d693f00b82d2f49a
Lara Dias <lara.dias@estudante.ufjf.br>
2024-12-16 17:35:31 -0300

    atualiza função data para buscar somente pelo mês e adiciona mais feriados a constante feriados

--- src/Calendario.tsx
+++ src/Calendario.tsx
@@ -4,28 +4,32 @@ import './Calendario.css';
 const Data: React.FC = () => {
     const feriados = [
       { data: "01/01", nome: "Ano Novo", mes:1 },
+      { data: "12/02", nome: "Carnaval", mes:2 },
+      { data: "13/02", nome: "Carnaval", mes:2 },
+      { data: "29/03", nome: "Paixão de Cristo", mes:3 },
+      { data: "21/04", nome: "Tiradentes", mes:4 },
       { data : "01/05", nome: "Dia do Trabalhador", mes:5},
+      { data : "30/05", nome: "Corpus Christi", mes:5},
       { data: "07/09", nome: "Independência do Brasil",mes:9 },
       { data: "12/10", nome: "Nossa Senhora Aparecida",mes:10 },
       { data: "02/11", nome: "Finados" , mes:11},
       { data: "15/11", nome: "Proclamação da República" , mes:11},
       {data: "20/11", nome:"Consciencia Negra",mes:11},
       { data: "25/12", nome: "Natal" , mes:12},
-      { data: "31/12", nome: "Ano Novo", mes:12},
     ];
   
     const [mesSelecionado, setMesSelecionado] = useState<number | null>(null);
 
     const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      const dataSelecionada = event.target.value; // Formato "YYYY-MM-DD"
-      const mes = new Date(dataSelecionada).getMonth() + 1; // Extrai o mês
+      const dataSelecionada = event.target.value; // Formato "YYYY-MM"
+      const mes = parseInt(dataSelecionada.split("-")[1],10); // Extrai o mês
       setMesSelecionado(mes);
     };
     const feriadosDoMes = feriados.filter((feriado) => feriado.mes === mesSelecionado);
     return (
         <div className="calendario-container">
       <div className="calendario-main">
-        <h1>Calendário de Viagens</h1>
+        <h1>Calendário de Feriados</h1>
         <div className="calendario-input">
           <input type="date" onChange={handleDateChange} />
         </div>
